2337f5b8cba5c3262875a467d4b99ef915742090
Virtrual-MM <bigjordon@163.com>
2017-10-28 14:58:10 +0800

    mod

--- subdir/pointer_test/point_test.c
+++ subdir/pointer_test/point_test.c
@@ -7,9 +7,23 @@ int main(void)
 	char (*c)[3][4];
 	char  *x[5];
 	char (*y)[5];
-	printf("%lx, %lx\n", a+1 , a);
-	printf("%lx, %lx\n", b+1 , b);
-	printf("%lx, %lx\n", c+1 , c);
+	char **test[3][4];
+	char (**magic)[3][2];
+	printf("%d %c %lx\n", sizeof(test), test, test+1);
+	// 96
+	printf("%d %c\n", sizeof(magic), magic);
+	//                                 char ** (*)[4]
+	printf("%lx, %lx, %lx, %lx\n", a+1 ,       a,           *a,    **a);
+	//                            char (*)[4], char (*)[4], char*, int  
+	/*
+	  the * operator give the last [] out , and & add the last [] up
+	*/
+	printf("%c, %lx, %lx, %lx\n", &a ,            a[0],   &a[0],       &a[0][0]);
+	//                            char (*)[3][4], char *, char (*)[4], char* 
+	printf("%lx, %lx, %lx, %lx, %lx\n", b+1 ,       b, *b,      **b,   ***b);
+	//                                  char * (*)[4], char **, char*, int
+	printf("%c, %lx, %lx, %lx\n", c+1 , c,        *c,          **c);
+	//                            char (*)[3][4], char (*)[4], char *
 	printf("%lx, %lx\n", x+1 , x);
 	printf("%lx, %lx\n", y+1 , y);
 	return 0;
