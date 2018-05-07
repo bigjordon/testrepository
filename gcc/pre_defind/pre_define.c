a48e70681a4559020eb44e1ec63936ed766c7595
MIXQ <jordon@127.0.0.1jordon>
2018-05-07 16:10:01 +0800

    mod

--- gcc/pre_defind/pre_define.c
+++ gcc/pre_defind/pre_define.c
@@ -1,6 +1,10 @@
 #define NUM 5
 #define STR1(R)  #R  
 #define STR2(R)  STR1(R)  
-STR1(NUM)
-STR2(NUM)
+"STR1(NUM)" STR1(NUM)
+"STR2(NUM)" STR2(NUM)
+"STR1(2)"   STR1(2)
+"STR2(2)"   STR2(2)
+"STR1(STR1-(NUM))"   STR1(STR1-(NUM))  不可嵌套
+
 char *string = "start  " STR2(NUM) " end";
